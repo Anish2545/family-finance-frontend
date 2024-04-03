1a12f685add1954fe40ae58e0d1f556df46e8263
Khushvipatel28 <khushvipatel.ec20@scet.ac.in>
2024-04-03 15:54:55 +0530

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+   {
     path: '',
+    loadChildren: () => import('./start/start.module').then( m => m.StartPageModule)
+  },
+  {
+    path: 'tab',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
+
+
 ];
 @NgModule({
   imports: [
