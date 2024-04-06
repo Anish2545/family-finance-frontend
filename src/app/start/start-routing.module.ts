93defe9280b70111d01feb927b403a2f73368cc8
dharmarajjardosh <dharmarajjardosh@gmail.com>
2024-04-06 13:50:57 +0530

    commit

--- src/app/start/start-routing.module.ts
+++ src/app/start/start-routing.module.ts
@@ -4,32 +4,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { StartPage } from './start.page';
 
 const routes: Routes = [
-   {
-
-    path: '',
-    loadChildren: () => import('./enterotp/enterotp.module').then( m => m.EnterotpPageModule)
-  },
-  {
-    path: '',
-    loadChildren: () => import('./otpverify2/otpverify2.module').then( m => m.Otpverify2PageModule)
-  },
-
-  {
-    path: 'signin',
-    loadChildren: () => import('./signin/signin.module').then( m => m.SigninPageModule)
-  },
-
   {
     path: '',
     loadChildren: () => import('./carousel/carousel.module').then( m => m.CarouselPageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
     path: 'sign-up',
     loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
-  }
+  },
+  {
+    path: 'enter-otp',
+    loadChildren: () => import('./enterotp/enterotp.module').then( m => m.EnterotpPageModule)
+  },
+  {
+    path: 'otp-verify',
+    loadChildren: () => import('./otpverify2/otpverify2.module').then( m => m.Otpverify2PageModule)
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 
 
 
