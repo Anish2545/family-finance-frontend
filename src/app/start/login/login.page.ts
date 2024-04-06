2b85ff1c82930fb9c26cfea5c2a587914d573a4b
dharmarajjardosh <dharmarajjardosh@gmail.com>
2024-04-06 15:14:35 +0530

    commit

--- src/app/start/login/login.page.ts
+++ src/app/start/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  form!: FormGroup;
+  type:boolean=true;
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.form=new FormGroup({
+      phone: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(10), Validators.maxLength(10)]
+      })
+  })
 
+}
+
+changeType() {
+  this.type = !this.type;
+}
+
+logIn() {
+  if(!this.form.valid) {
+    this.form.markAllAsTouched();
+    return;
+  }
+  console.log(this.form.value);
+}
 }
