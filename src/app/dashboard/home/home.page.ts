9155b02704206cd4d41b38f0b8a126d73b98aac8
dharmarajjardosh <dharmarajjardosh@gmail.com>
2024-05-08 17:52:23 +0530

    dashboard

--- src/app/dashboard/home/home.page.ts
+++ src/app/dashboard/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtilService } from 'src/app/util.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage  {
+  totalincome: any;
+  totalexpense: any;
+  balance: any;
+  constructor(private utilService: UtilService) {
+    this.totalIncome()
+    }
 
-  constructor() {  }
+  totalIncome() {
+    let reqBody = {
+      totalincome: this.totalincome,
+      totalexpense: this.totalexpense,
+      balance:this.balance,
+    }
+    this.utilService.callPostApi(reqBody, 'transaction/transactionlist').subscribe(async result => {
+      if (result.flag) {
+        this.totalincome = result.data;
+        this.totalexpense = result.data;
+        this.balance = result.data ;
+      }
+    })
+  }
 }
